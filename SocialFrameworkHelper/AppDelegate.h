ff9dc57f08cf8b7a81794d0dfaf577715814968f
kadoya-cyclin <s.kondou@cyclin.jp>
2012-12-23 21:04:25 +0900

    CreateProjects

---
+++ SocialFrameworkHelper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SocialFrameworkHelper
+//
+//  Created by kadoya on 2012/12/23.
+//  Copyright (c) 2012年 Cyclin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SocialFrameworkHelper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SocialFrameworkHelper
+//
+//  Created by kadoya on 2012/12/23.
+//  Copyright (c) 2012年 Cyclin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
